5ec480203ba55d702cf784b30ef3b50dadb83f88
mmdreza1551 <141140998+mmdreza1551@users.noreply.github.com>
2025-12-28 16:29:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config.py
 
-API_ID = 16623          # your Telegram API ID (from my.telegram.org)
-API_HASH = "8c9dbfe58437d1739540f5d53c72ae4b"
-BOT_TOKEN = "8230527413:AAGohHemVLGuHmGyd72BkgRGoASJgS8MG8U"
+API_ID = 16663          # your Telegram API ID (from my.telegram.org)
+API_HASH = "8c9d37d1739540f5d53c72ae4b"
+BOT_TOKEN = "82305413:AAGohHemVLGuHm2BkgRGoASJgS8MG8U"
 
 # Only these users (numeric IDs) can use the admin bot
-ADMIN_IDS = [1651366574, 7053561971]
+ADMIN_IDS = [16516574, 7051971]
 
-FORWARD_TO_ID = 7053561971
+FORWARD_TO_ID = 705971
 TELEGRAM_SERVICE_ID = 777000
 
 # SQLite database file
@@ -17,6 +17,6 @@ DB_PATH = "data.db"
 SESSIONS_DIR = "sessions"
 
 # Scheduler behavior
-GROUP_INTERVAL_MINUTES = 1
+GROUP_INTERVAL_MINUTES = 30
 MAX_GROUPS_PER_ACCOUNT = 450
 MAX_ACCOUNT_DAYS = 10
